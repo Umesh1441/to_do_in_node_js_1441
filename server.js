0b64b9edfe88250aa353b5b2ccd266657b91f871
Umesh1441 <umeshtak34@gmail.com>
2023-12-22 12:32:24 +0530

    fixed engine

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ import { connectDB } from "./data/database.js";
 connectDB();
 // console.log(process.env.PORT);
 app.listen(process.env.PORT,()=>{
-    console.log("server is working...");
-})
+    console.log(`server is working on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
